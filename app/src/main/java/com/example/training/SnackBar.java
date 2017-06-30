1470ae87053a6a8ff71c5cd2b8b4eb4a14cf29c4
Developer <igorkostan@gmail.com>
2017-06-29 23:26:11 -0700

    New SnackBar

---
+++ app/src/main/java/com/example/training/SnackBar.java
@@ -0,0 +1,46 @@
+package com.example.training;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class SnackBar extends AppCompatActivity {
+
+    Button buttonClose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snack_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        this.setTitle("SnackBar Demo");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Close button
+        buttonClose = (Button)findViewById(R.id.buttonClose);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Close the view
+                finish();
+            }
+        });
+    }
+
+
+    //END
+}
